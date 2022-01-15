e56ec7c81ac708d391f5724c334f132bb0447b4e
ede <edgar.soldin@web.de>
2022-01-15 22:03:40 +0100

    rework vertexSymbols integration
    refined enablechecks, added vertexSymbols error

--- src/main/java/org/openjump/advancedtools/block/SaveBlockPlugIn.java
+++ src/main/java/org/openjump/advancedtools/block/SaveBlockPlugIn.java
@@ -3,54 +3,55 @@ package org.openjump.advancedtools.block;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.io.File;
+import java.lang.reflect.Method;
 import java.util.Collection;
 
 import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.openjump.advancedtools.CadExtension;
 import org.openjump.advancedtools.icon.IconLoader;
 import org.saig.core.gui.swing.sldeditor.util.FormUtils;
 
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.Geometry;
-import com.vividsolutions.jump.I18N;
+import com.cadplan.vertex_symbols.jump.VertexSymbolsExtension;
 import com.vividsolutions.jump.feature.Feature;
 import com.vividsolutions.jump.geom.CoordUtil;
-import com.vividsolutions.jump.workbench.JUMPWorkbench;
 import com.vividsolutions.jump.workbench.Logger;
 import com.vividsolutions.jump.workbench.WorkbenchContext;
 import com.vividsolutions.jump.workbench.plugin.AbstractPlugIn;
+import com.vividsolutions.jump.workbench.plugin.EnableCheck;
 import com.vividsolutions.jump.workbench.plugin.EnableCheckFactory;
 import com.vividsolutions.jump.workbench.plugin.MultiEnableCheck;
 import com.vividsolutions.jump.workbench.plugin.PlugInContext;
 import com.vividsolutions.jump.workbench.ui.GUIUtil;
 import com.vividsolutions.jump.workbench.ui.MultiInputDialog;
-import com.vividsolutions.jump.workbench.ui.TaskFrame;
 
 public class SaveBlockPlugIn extends AbstractPlugIn {
 
-    private static final I18N i18n = I18N.getInstance("org.openjump.advancedtools");
-
     public static ImageIcon ICON = IconLoader.icon("textblock/block_save.png");
 
-    private static final String NAME = i18n
+    private static final String NAME = CadExtension.I18N
         .get("org.openjump.core.ui.plugins.block.SaveBlockPlugIn");
-    private static final String Warning1 = i18n
+    private static final String Warning1 = CadExtension.I18N
         .get("org.openjump.core.ui.plugins.block.SaveBlockPlugIn.description-title");
-    private static final String Warning2 = i18n
+    private static final String Warning2 = CadExtension.I18N
         .get("org.openjump.core.ui.plugins.block.SaveBlockPlugIn.warning-positive");
-    private static final String Warning3 = i18n
+    private static final String Warning3 = CadExtension.I18N
         .get("org.openjump.core.ui.plugins.block.SaveBlockPlugIn.warning-negative");
-    private static final String Description = i18n
+    private static final String Description = CadExtension.I18N
         .get("org.openjump.core.ui.plugins.block.SaveBlockPlugIn.description");
 
     private final BlockPanel blockPanel;
     private final JTextField jTextField_wktOut;
 
 
-    private String blockFolder = "VertexImages";
+    private String blockFolderName = "VertexImages";
+    private File blockFolderPath = null;
     private Geometry geomSelected = null;
 
     public SaveBlockPlugIn(BlockPanel blockPanel) {
@@ -76,8 +77,8 @@ public class SaveBlockPlugIn extends AbstractPlugIn {
     }
 
     @Override
-    public void initialize(PlugInContext context) {
-
+    public void initialize(PlugInContext context) throws Exception {
+      super.initialize(context);
     }
 
     public static MultiEnableCheck createEnableCheck(
@@ -94,143 +95,140 @@ public class SaveBlockPlugIn extends AbstractPlugIn {
 
     @Override
     public boolean execute(PlugInContext context) throws Exception {
-        reportNothingToUndoYet(context);
+      String msg;
+      if ((msg = getEnableCheck().check(null)) != null) {
+        context.getWorkbenchFrame().warnUser(msg);
+        return false;
+      }
 
-        if (!(JUMPWorkbench.getInstance().getFrame().getActiveInternalFrame() instanceof TaskFrame)) {
-            JUMPWorkbench
-                    .getInstance()
-                    .getFrame()
-                    .warnUser(
-                            I18N.JUMP.get("com.vividsolutions.jump.workbench.plugin.A-Task-Window-must-be-active"));
-            return false;
-        } else {
-            try {
-                Collection<Feature> selectedFeatures = context.getLayerViewPanel()
-                        .getSelectionManager()
-                        .createFeaturesFromSelectedItems();
-                if (selectedFeatures.size() > 1 || selectedFeatures.size() == 0) {
-                    context.getWorkbenchFrame().warnUser(Warning1 + "!");
-                    return false;
-                }
-                File pluginDir = context.getWorkbenchContext().getWorkbench()
-                        .getPlugInManager().getPlugInDirectory();
-                String wd = pluginDir.getAbsolutePath();
-                String filenamedir;
-                MultiInputDialog dialog = new MultiInputDialog(
-                        context.getWorkbenchFrame(), NAME, true);
+      reportNothingToUndoYet(context);
 
-                dialog.addRow(createMainPanel());
+      Collection<Feature> selectedFeatures = context.getLayerViewPanel().getSelectionManager()
+          .createFeaturesFromSelectedItems();
+      if (selectedFeatures.size() > 1 || selectedFeatures.size() == 0) {
+        context.getWorkbenchFrame().warnUser(Warning1 + "!");
+        return false;
+      }
 
-                GUIUtil.centreOnWindow(dialog);
-                dialog.setVisible(true);
+      MultiInputDialog dialog = new MultiInputDialog(context.getWorkbenchFrame(), NAME, true);
 
-                if (!dialog.wasOKPressed()) {
-                    return false;
-                }
-                if (jTextField_wktOut.getText().isEmpty()) {
-                    context.getWorkbenchFrame().warnUser(Warning3);
-                    return false;
-                }
+      dialog.addRow(createMainPanel());
 
-                else {
+      GUIUtil.centreOnWindow(dialog);
+      dialog.setVisible(true);
 
-                    String filename = jTextField_wktOut.getText();
+      if (!dialog.wasOKPressed()) {
+        return false;
+      }
 
-                    for (Feature featureSelected : selectedFeatures) {
-                        geomSelected = featureSelected.getGeometry();
-                    }
+      if (jTextField_wktOut.getText().isEmpty()) {
+        context.getWorkbenchFrame().warnUser(Warning3);
+        return false;
+      }
 
-                    //   Geometry targetGeom = new GeometryFactory()
-                    //           .createLineString(new Coordinate[] {
-                    //                   new Coordinate(0, 0),
-                    //                   new Coordinate(0, 8.49) });
-                    //    double targetLength = targetGeom.getLength();
-                    //   double scale = 0;
+      String filename = jTextField_wktOut.getText();
 
-                    //    Envelope envSelected = geomSelected.getEnvelopeInternal();
+      for (Feature featureSelected : selectedFeatures) {
+        geomSelected = featureSelected.getGeometry();
+      }
 
-                    //     Geometry sourceGeom = new GeometryFactory()
-                    //             .createLineString(new Coordinate[] {
-                    //                     new Coordinate(envSelected.getMinX(),
-                    //                             envSelected.getMinY()),
-                    //                     new Coordinate(envSelected.getMaxY(),
-                    //                             envSelected.getMaxY()) });
-                    //   double sourceLength = sourceGeom.getLength();
+      // Geometry targetGeom = new GeometryFactory()
+      // .createLineString(new Coordinate[] {
+      // new Coordinate(0, 0),
+      // new Coordinate(0, 8.49) });
+      // double targetLength = targetGeom.getLength();
+      // double scale = 0;
 
-                    /*
-                     * if (sourceLength>22){ scale =
-                     * (targetLength/sourceLength)*100; } else{ scale =
-                     * (22/sourceLength)*100; }
-                     */
-                    Geometry newGeom = geomSelected.copy();
-                    // Get the centroid coordinates of the geometry
-                    Coordinate coord = newGeom.getEnvelope().getCentroid()
-                            .getCoordinate();
+      // Envelope envSelected = geomSelected.getEnvelopeInternal();
 
-                    // Calculate the displacement of the geometry
-                    Coordinate displacement = CoordUtil.subtract(
-                            new Coordinate(0, 0), coord);
+      // Geometry sourceGeom = new GeometryFactory()
+      // .createLineString(new Coordinate[] {
+      // new Coordinate(envSelected.getMinX(),
+      // envSelected.getMinY()),
+      // new Coordinate(envSelected.getMaxY(),
+      // envSelected.getMaxY()) });
+      // double sourceLength = sourceGeom.getLength();
 
-                    //   scale = (targetLength / sourceLength);
+      /*
+       * if (sourceLength>22){ scale =
+       * (targetLength/sourceLength)*100; } else{ scale =
+       * (22/sourceLength)*100; }
+       */
+      Geometry newGeom = geomSelected.copy();
+      // Get the centroid coordinates of the geometry
+      Coordinate coord = newGeom.getEnvelope().getCentroid().getCoordinate();
 
-                    GeometryUtils.centerGeometry(newGeom, displacement);
-                    //   GeometryUtils.scaleGeometry(newGeom, scale);
+      // Calculate the displacement of the geometry
+      Coordinate displacement = CoordUtil.subtract(new Coordinate(0, 0), coord);
 
-                    filenamedir = wd + File.separator + blockFolder
-                            + File.separator + filename;
-                    File f = new File(filenamedir);
-                    if (f.exists() && f.isDirectory()) {
-                        GeometryUtils.writeToFile(newGeom, filenamedir);
-                    } else {
-                        File dir = new File(wd + File.separator + blockFolder);
-                        dir.mkdir();
-                        filenamedir = dir.getAbsolutePath() + File.separator
-                                + filename;
-                        GeometryUtils.writeToFile(newGeom, filenamedir);
-                    }
-                	if (checkSymbolLibrary()) {
-						com.cadplan.jump.utils.LoadSymbolFiles loadSymbols = new com.cadplan.jump.utils.LoadSymbolFiles(context);
-						loadSymbols.start();
-					}
-                    context.getWorkbenchFrame().setStatusMessage(Warning2);
+      // scale = (targetLength / sourceLength);
 
-                    if (blockPanel != null) {
-                        blockPanel.initComboBox(context);
-                    }
-                }
+      GeometryUtils.centerGeometry(newGeom, displacement);
+      // GeometryUtils.scaleGeometry(newGeom, scale);
 
+      // TODO: use reflection? remove dependency from pom?
+      File blockFolder = VertexSymbolsExtension.getVertexImagesFolder(context.getWorkbenchContext());
+      // String filenamedir = wd + File.separator + blockFolder
+      // + File.separator + filename;
+      // File f = new File(filenamedir);
+      if (blockFolder.isDirectory()) {
+        GeometryUtils.writeToFile(newGeom, new File( blockFolder, filename ).getAbsolutePath());
+      }
 
-            } catch (Exception ex) {
-                Logger.warn(ex);
-            }
-            return true;
-        }
+      // only add if Vertex-Symbols is installed
+      Class<?> c = checkSymbolLibrary();
+      if (c != null) {
+        // the same code as below, but using reflection
+        Object loadSymbols = c.getConstructor(PlugInContext.class).newInstance(context);
+        Method m = c.getMethod("start");
+        m.invoke(loadSymbols, null);
+        // LoadSymbolFiles loadSymbols = new LoadSymbolFiles(context);
+        // loadSymbols.start();
+      }
+      context.getWorkbenchFrame().setStatusMessage(Warning2);
+
+      if (blockPanel != null) {
+        blockPanel.initComboBox(context);
+      }
+
+      return true;
     }
 
-  boolean checkSymbolLibrary() {
-		ClassLoader cl = this.getClass().getClassLoader();
-		Class<?> c = null;
-		try {
-			c = cl.loadClass("com.cadplan.jump.utils.LoadSymbolFiles");
-		}
-		catch (ClassNotFoundException e) {
-			Logger.warn("Could not load Symbol file library", e);
-		}
+    private Class checkSymbolLibrary() {
+      ClassLoader cl = this.getClass().getClassLoader();
+      Class<?> c = null;
+      try {
+        c = cl.loadClass("com.cadplan.vertex_symbols.jump.utils.LoadSymbolFiles");
+      } catch (ClassNotFoundException e) {
+        Logger.warn("Could not load Symbol file library", e);
+      }
 
-		return c != null;
-	}
-    
-    
+      return c;
+    }
+
     private JPanel createMainPanel() {
         JPanel jpanel = new JPanel(new GridBagLayout());
         JLabel jLabel = new JLabel();
-        String OUTPUT_FILE = i18n
+        String OUTPUT_FILE = CadExtension.I18N
             .get("org.openjump.core.ui.plugins.block.SaveBlockPlugIn.dialog");
         jLabel.setText(OUTPUT_FILE);
         jTextField_wktOut.setEditable(true);
         jTextField_wktOut.setPreferredSize(new Dimension(250, 20));
         FormUtils.addRowInGBL(jpanel, 3, 0, OUTPUT_FILE, jTextField_wktOut);
         return jpanel;
+    }
+
+    @Override
+    public EnableCheck getEnableCheck() {
+      EnableCheckFactory factory = EnableCheckFactory.getInstance(getWorkbenchContext());
+      return new MultiEnableCheck().add(factory.createTaskWindowMustBeActiveCheck()).add(new EnableCheck() {
+        @Override
+        public String check(JComponent component) {
+          if (checkSymbolLibrary()==null)
+            return CadExtension.I18N.get("org.openjump.advancedtools.vertex-symbols.missing");
+          return null;
+        }
+      });
     }
 
 }
